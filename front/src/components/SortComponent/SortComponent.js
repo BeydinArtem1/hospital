d7406f2ae61134d2e04fd75f4aa193cfae29e7bc
Artem Beydin <artbeidin.exceedteam@gmail.com>
2021-11-24 17:36:01 +0300

    add sort function

---
+++ front/src/components/SortComponent/SortComponent.js
@@ -0,0 +1,95 @@
+import React, { useState } from 'react';
+import {
+  Select,
+  MenuItem,
+  FormControl
+} from '@mui/material';
+import './SortComponent.scss';
+
+const SortComponent = ({ appointments, setAppointment }) => {
+  const [section, setSection] = useState('');
+  const [vector, setVector] = useState('ascending');
+  const direction = ['ascending', 'descending'];
+  const selectors = ['name', 'doc', 'date', 'none'];
+
+  const sortAppointment = (field, dir) => {
+    console.log(appointments)
+    if (field === 'none') field = '_id';
+    appointments.sort((a, b) => a[field] > b[field] ? 1 : a[field] < b[field] ? -1 : 0);
+    if (dir === 'descending') appointments.reverse();
+    setAppointment([...appointments]);
+  }
+
+  const handleSortValue = (e) => {
+    setVector(e);
+    sortAppointment(section, e);
+  }
+
+  const handleField = (e) => {
+    setSection(e);
+    sortAppointment(e, vector);
+  }
+
+  return (
+    <div className='sort-container'>
+      <div className='sort-by'>
+        <p>Сортировать по:</p>
+        <FormControl
+          fullWidth
+          className='sort-form'
+        >
+          <Select
+            labelId="demo-simple-select-standard-label"
+            id="demo-simple-select-standard"
+            value={section}
+            onChange={(e) => handleField(e.target.value)}
+          >
+            {
+              selectors.map((value, index) =>
+                <MenuItem
+                  key={`selector-${index}`}
+                  value={value}
+                >
+                  {value}
+                </MenuItem>
+              )
+            }
+          </Select>
+        </FormControl>
+      </div>
+      {
+        section && (section !== 'none')
+          ?
+          <div className='sort-direction'>
+            <p>Направление:</p>
+            <FormControl
+              fullWidth
+              className='vector'
+            >
+              <Select
+                labelId="demo-simple-select-standard-label"
+                id="demo-simple-select-standard"
+                value={vector}
+                onChange={(e) => handleSortValue(e.target.value)}
+              >
+                {
+                  direction.map((value, index) =>
+                    <MenuItem
+                      key={`direction-${index}`}
+                      value={value}
+                    >
+                      {value}
+                    </MenuItem>
+                  )
+                }
+              </Select>
+            </FormControl>
+          </div>
+          :
+          <></>
+      }
+    </div>
+  )
+}
+
+export default SortComponent
